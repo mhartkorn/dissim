6b2cbd89bccf4e09fa5b52deb07f73520a51f673
Martin Hartkorn <github@hartkorn.net>
2015-06-02 14:59:43 +0200

    Switched to ggplot2 and added table for raw data

--- diceroll/server.R
+++ diceroll/server.R
@@ -1,46 +1,51 @@
 library(shiny)
+library(ggplot2)
 
 samplesize = 20000
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   vars <- reactiveValues(throws1 = sample(1:6, samplesize * 2, replace =  T),
-                         throws2 = numeric(samplesize),
-                         bins1 = seq(1, 6, length.out = 7), 
-                         bins2 = seq(1, 12, length.out = 13))
+                         throws2 = numeric(samplesize))
+  
+  observe({
+      vars$throws1 <- sample(1:input$numSides, samplesize * 2, replace = T)
+  })
+  
+  
+  #
+  ## 1 Dice
+  #
   
   computeThrows1 <- reactive({
     vars$throws1[1:input$numThrows]
   })
   
-  observe({
-      vars$throws1 <- sample(1:input$numSides, samplesize * 2, replace = T)      
-      vars$bins1 <- seq(1, input$numSides, length.out = input$numSides + 1)
-      vars$bins2 <- seq(1, input$numSides * 2, length.out = input$numSides * 2 + 1)
-  })
-  
   output$distPlot <- renderPlot({
-    # draw the histogram with the specified number of bins
-    hist(computeThrows1(), breaks = vars$bins1, col = 'darkgray', border = 'white',
-         xlab = 'Augenzahl', ylab = 'Häufigkeit', main = "Absolute Häufigkeit der Augenzahl")
+    histData = data.frame(x = computeThrows1())
+    ggplot(histData, aes(x)) +
+      geom_histogram(binwidth = 1, origin = -0.5, col = "white", fill = "darkgray") +
+      scale_x_continuous(breaks = 1:input$numSides) +
+      ylab("Absolute Häufigkeit") +
+      xlab("Augenzahl") +
+      theme_classic()
   })
   
   output$densityPlot <- renderPlot({
-    # draw the histogram with the specified number of bins
-    dens <- density(computeThrows1())
-    maxDens <- max(dens$y)
-
-    hist(computeThrows1(), breaks = vars$bins1, col = 'darkgrey', border = 'white', probability = T,
-         xlab = 'Augenzahl', ylab = 'Häufigkeit', main = "Relative Häufigkeit der Augenzahl",
-         ylim = c(0, maxDens + maxDens * 0.05))
-    lines(dens, col = 'red', lwd = 3)
+    histData = data.frame(x = computeThrows1())
+    ggplot(histData, aes(x, y = ..density..)) +
+      geom_histogram(binwidth = 1, origin = -0.5, col = "white", fill = "darkgray") +
+      geom_density() +
+      scale_x_continuous(breaks = 1:input$numSides, name = "Augenzahl") +
+      ylab("Relative Häufigkeit") +
+      xlab("Augenzahl") +
+      theme_classic()
   })
   
   output$data <- renderText({paste("Varianz =", round(var(computeThrows1()), 3), "|", "Durchschnitt =", round(mean(computeThrows1()), 3))})
   
   
   #
-  ## 2 Würfel
+  ## 2 Dice
   #
   
   computeThrows2 <- reactive({
@@ -54,20 +59,24 @@ shinyServer(function(input, output) {
   })
   
   output$distPlot2 <- renderPlot({
-    # draw the histogram with the specified number of bins
-    hist(computeThrows2(), breaks = vars$bins2, col = 'darkgray', border = 'white',
-         xlab = 'Augenzahl', ylab = 'Häufigkeit', main = "Absolute Häufigkeit der Augenzahl")
+    histData = data.frame(x = computeThrows2())
+    ggplot(histData, aes(x)) +
+      geom_histogram(binwidth = 1, origin = -0.5, col = "white", fill = "darkgray") +
+      scale_x_continuous(breaks = 1:(input$numSides * 2)) +
+      ylab("Absolute Häufigkeit") +
+      xlab("Augenzahl") +
+      theme_classic()
   })
   
   output$densityPlot2 <- renderPlot({
-    # draw the histogram with the specified number of bins
-    dens <- density(computeThrows2())
-    maxDens <- max(dens$y)
-    
-    hist(computeThrows2(), breaks = vars$bins2, col = 'darkgrey', border = 'white', probability = T,
-         xlab = 'Augenzahl', ylab = 'Häufigkeit', main = "Relative Häufigkeit der Augenzahl",
-         ylim = c(0, maxDens + maxDens * 0.05))
-    lines(dens, col = 'red', lwd = 3)
+    histData = data.frame(x = computeThrows2())
+    ggplot(histData, aes(x = x, y = ..density..)) +
+      geom_histogram(binwidth = 1, origin = -0.5, col = "white", fill = "darkgray") +
+      geom_density(name = "Relative Häufigkeit") +
+      scale_x_continuous(breaks = 1:(input$numSides * 2)) +
+      ylab("Relative Häufigkeit") +
+      xlab("Augenzahl") +
+      theme_classic()
   })
   
   output$dataTable2 <- renderDataTable({
@@ -85,6 +94,6 @@ shinyServer(function(input, output) {
   #
   
   output$raw <- renderDataTable({
-    vars$throws
+    table <- data.frame(roll1 = vars$throws1, roll2 = computeThrows2())
   })
 })
